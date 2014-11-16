6003f74d62fc8493aa949f8fd8ca59f3b96d0b59
Steven Das <steven@zirtual.com>
2014-11-16 12:22:32 -0500

    Substituted project description for a FIXME.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojserve "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "This is intended as a learning project for both Clojure and the fundamentals of web servers; the goal is to write a Clojure program which accepts nearly any type of HTTP request and returns the appropriate HTTP response.  NOT intended for production use."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
