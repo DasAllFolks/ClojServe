6a9008ba927d3c36d85f3468741739942d625d05
Steven Das <steven@zirtual.com>
2014-11-16 11:58:31 -0500

    Initial commit of Leiningen project template.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojserve
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojserve/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojserve.core-test
+  (:require [clojure.test :refer :all]
+            [clojserve.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
