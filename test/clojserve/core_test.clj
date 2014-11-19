484ae1eaa82893887e1560fc9792c95e8c4f29d3
Steven Das <steven@zirtual.com>
2014-11-18 21:16:10 -0500

    Some playing around with unit tests.

--- test/clojserve/core_test.clj
+++ test/clojserve/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [clojserve.core :refer :all]))
 
-(deftest a-test
+(deftest fail-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest success-test
+  (testing "Whereas I work."
+    (is (= 1 1))))
