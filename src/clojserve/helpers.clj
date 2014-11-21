0a66d527a32a5e0b4d2561039a0ac048ffd6a81d
Steven Das <steven@zirtual.com>
2014-11-21 08:15:38 -0500

    Start of parse string method.

--- src/clojserve/helpers.clj
+++ src/clojserve/helpers.clj
@@ -1,10 +1,16 @@
 (ns clojserve.core
   (:gen-class))
 
+(defn tokenize-string
+  "Separates a string into a list of string tokens given a separator character"
+  [separator string]
+  ; XXXX: Write me!
+  nil )
+
 (defn process-request-line
   "Separates out parameters for the HTTP request line"
   [request-line]
   ; XXXX: Combine parameters which are just characters right now.
   ; XXXX: And then what to do with all of these parameters?
-  (nil 
+  ([] nil)
   (filter #(not (or (= % \space) (= % \return) (= % \newline))) request-line))
