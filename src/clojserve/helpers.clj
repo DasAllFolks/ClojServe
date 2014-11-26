b96ba50213f660136dd3c7be60046ee4dc1a59d0
Steven Das <steven@zirtual.com>
2014-11-25 22:56:34 -0500

    Fixed capitalization bug and added todos for unit tests and HTTP type request check.

--- src/clojserve/helpers.clj
+++ src/clojserve/helpers.clj
@@ -3,13 +3,16 @@
 (require '[clojure.string :as string])
 
 (def request-type?
+  ; XXXX: Write unit tests
+  ; XXXX: Make sure we've covered all appropriate HTTP methods
   "Checks if input uppercase string corresponds to an HTTP request type"
   (set '("GET" "POST" "PUT" "HEAD" "TRACE" "OPTIONS" "DELETE" "CONNECT" "PATCH")))
 
 (defn is-valid-request-type
   "Returns true for valid request types, false otherwise (case-insensitive)"
   [request-type]
-  (not (= nil (request-type? (string/capitalize request-type)))))
+  ; XXXX: Write unit tests
+  (not (= nil (request-type? (string/upper-case request-type)))))
 
 (defn process-request-line
   "Separates out parameters for the HTTP request line"
