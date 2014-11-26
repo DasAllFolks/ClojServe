0222138a588f5f0f95a450134327ce70ad1cf237
Steven Das <steven@zirtual.com>
2014-11-25 22:44:30 -0500

    Capitalized method names, wrote method to convert output of method search to boolean values.

--- src/clojserve/helpers.clj
+++ src/clojserve/helpers.clj
@@ -1,10 +1,15 @@
 (ns clojserve.core
   (:gen-class))
+(require '[clojure.string :as string])
 
 (def request-type?
-  "Checks if input lowercase string corresponds to an HTTP request type"
-  (set '("get" "post" "put" "head" "trace" "options" "delete" "connect" "patch")))
+  "Checks if input uppercase string corresponds to an HTTP request type"
+  (set '("GET" "POST" "PUT" "HEAD" "TRACE" "OPTIONS" "DELETE" "CONNECT" "PATCH")))
 
+(defn is-valid-request-type
+  "Returns true for valid request types, false otherwise (case-insensitive)"
+  [request-type]
+  (not (= (nil (request-type? (string/capitalize request-type))))))
 
 (defn process-request-line
   "Separates out parameters for the HTTP request line"
